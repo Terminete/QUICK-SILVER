60254b1fbbf45cbcf10b38ba4cf556b711066f83
Terminete <paulmuli878@gmail.com>
2026-03-02 12:19:13 +0300

    added search button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useCart } from "../Context/CartContext";
 import { useAuth } from "../Context/AuthContext";
+import { useState } from "react";
 const Navbar = () => {
   const { cart } = useCart();
   const { user, logout } = useAuth();
-
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/?search=${search}`);
+  }
   return (
     <>
       <nav className="w-full h-22 flex justify-between items-center  bg-green-500 text-white py-2 px-4  overscroll-contain ">
         <h1 className="">
           <Link to="/ " className="font-bold text-2xl  ">QUICK SILVER GAS POINT</Link>
         </h1>
+         <form onSubmit={handleSearch} className="flex gap-2">
+        <input
+          type="text"
+          placeholder="Search gas..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="border px-3 py-1 rounded"
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-600 text-white px-4 py-1 rounded"
+        >
+          Search
+        </button>
+      </form>
         <div className="space-x-4">
           <Link to="/">Home</Link>
           <Link to="/cart">Cart ({cart.length})</Link>
